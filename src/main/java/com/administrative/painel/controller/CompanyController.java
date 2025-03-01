e4d3564e712b586108db1bf6703035782342fb31
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2025-03-01 18:12:56 -0300

    Alterações na conexão com banco

--- src/main/java/com/administrative/painel/controller/CompanyController.java
+++ src/main/java/com/administrative/painel/controller/CompanyController.java
@@ -7,7 +7,6 @@ import com.administrative.painel.service.CompanyService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
